6ef6a5fc79c9cda38744b44f8b64295789dac881
FallenTeen <vlamingvlaming0@gmail.com>
2024-06-24 09:09:21 +0700

    nambah login facebook dan gmail

--- lib/halaman/hal_login.dart
+++ lib/halaman/hal_login.dart
@@ -1,12 +1,14 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:flutter_facebook_auth/flutter_facebook_auth.dart';
 import 'package:maxtrip/components/loginbutton.dart';
 import 'package:maxtrip/components/logintextfield.dart';
 import 'package:maxtrip/components/pesan_error.dart';
-import 'package:maxtrip/halaman/mainpage.dart'; // Import HalHome widget
+import 'package:maxtrip/halaman/mainpage.dart';
 
 class HalLogin extends StatelessWidget {
-  HalLogin({Key? key}) : super(key: key); // Fix constructor syntax
+  HalLogin({Key? key}) : super(key: key);
 
   final emailController = TextEditingController();
   final passwordController = TextEditingController();
@@ -17,9 +19,7 @@ class HalLogin extends StatelessWidget {
         email: emailController.text,
         password: passwordController.text,
       );
-      // Show success message dialog
       _showSuccessDialog(context);
-      // Navigate to the next page after a delay
       Future.delayed(Duration(seconds: 2), () {
         Navigator.pushReplacement(
           context,
@@ -29,7 +29,6 @@ class HalLogin extends StatelessWidget {
     } on FirebaseAuthException catch (e) {
       print('Error: ${e.message}');
       String errorMessage = _getErrorMessage(e.code);
-      // Show error dialog with message
       showDialog(
         context: context,
         builder: (context) => AlertDialog(
@@ -84,6 +83,99 @@ class HalLogin extends StatelessWidget {
         return ErrorMessages.wrongPassword;
       default:
         return 'An error occurred.';
+    }
+  }
+
+  Future<void> signInWithGoogle(BuildContext context) async {
+    try {
+      final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
+      if (googleUser == null) {
+        return; // The user canceled the sign-in
+      }
+      final GoogleSignInAuthentication googleAuth =
+          await googleUser.authentication;
+      final AuthCredential credential = GoogleAuthProvider.credential(
+        accessToken: googleAuth.accessToken,
+        idToken: googleAuth.idToken,
+      );
+      await FirebaseAuth.instance.signInWithCredential(credential);
+      _showSuccessDialog(context);
+      Future.delayed(Duration(seconds: 2), () {
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(builder: (context) => MainPage()),
+        );
+      });
+    } catch (e) {
+      print('Error: $e');
+      showDialog(
+        context: context,
+        builder: (context) => AlertDialog(
+          title: Text('Login Failed'),
+          content: Text('Failed to sign in with Google: $e'),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: Text('OK'),
+            ),
+          ],
+        ),
+      );
+    }
+  }
+
+  Future<void> signInWithFacebook(BuildContext context) async {
+    try {
+      final LoginResult result = await FacebookAuth.instance.login();
+      if (result.status == LoginStatus.success) {
+        final AccessToken accessToken = result.accessToken!;
+        final AuthCredential credential =
+            FacebookAuthProvider.credential(accessToken.token);
+        await FirebaseAuth.instance.signInWithCredential(credential);
+        _showSuccessDialog(context);
+        Future.delayed(Duration(seconds: 2), () {
+          Navigator.pushReplacement(
+            context,
+            MaterialPageRoute(builder: (context) => MainPage()),
+          );
+        });
+      } else {
+        print('Error: ${result.message}');
+        showDialog(
+          context: context,
+          builder: (context) => AlertDialog(
+            title: Text('Login Failed'),
+            content: Text('Failed to sign in with Facebook: ${result.message}'),
+            actions: [
+              TextButton(
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+                child: Text('OK'),
+              ),
+            ],
+          ),
+        );
+      }
+    } catch (e) {
+      print('Error: $e');
+      showDialog(
+        context: context,
+        builder: (context) => AlertDialog(
+          title: Text('Login Failed'),
+          content: Text('Failed to sign in with Facebook: $e'),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: Text('OK'),
+            ),
+          ],
+        ),
+      );
     }
   }
 
@@ -152,8 +244,7 @@ class HalLogin extends StatelessWidget {
                       ],
                     ),
                     MyButton(
-                      onTap: () =>
-                          loginUser(context), // Call loginUser with context
+                      onTap: () => loginUser(context),
                     ),
                     SizedBox(height: 15),
                     Text(
@@ -167,16 +258,22 @@ class HalLogin extends StatelessWidget {
                     Row(
                       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                       children: [
-                        Image.asset(
-                          'lib/images/google.png',
-                          width: 50,
-                          height: 50,
+                        GestureDetector(
+                          onTap: () => signInWithGoogle(context),
+                          child: Image.asset(
+                            'lib/images/google.png',
+                            width: 50,
+                            height: 50,
+                          ),
                         ),
-                        Image.asset(
-                          'lib/images/facebook.png',
-                          width: 50,
-                          height: 50,
-                        )
+                        GestureDetector(
+                          onTap: () => signInWithFacebook(context),
+                          child: Image.asset(
+                            'lib/images/facebook.png',
+                            width: 50,
+                            height: 50,
+                          ),
+                        ),
                       ],
                     ),
                   ],
