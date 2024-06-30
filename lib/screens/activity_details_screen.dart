d2402c210d7c07dea0c9dc9b2012288b9e4c589e
NajibNurdiansyah <najibnurd@gmail.com>
2024-06-30 18:49:23 +0700

    modif activity_details_screen = activity

--- lib/screens/activity_details_screen.dart
+++ lib/screens/activity_details_screen.dart
@@ -14,11 +14,19 @@ class ActivityDetailsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        _ActivityImage(activity: activity),
-        _ActivityInformation(activity: activity),
-      ],
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(activity.title),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            _ActivityImage(activity: activity),
+            _ActivityInformation(activity: activity),
+          ],
+        ),
+      ),
     );
   }
 }
@@ -55,82 +63,76 @@ class _ActivityInformation extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Expanded(
-      child: Container(
-        padding: const EdgeInsets.only(
-          left: 20,
-          right: 20,
-          bottom: 40,
-        ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              activity.title,
-              style: Theme.of(context)
-                  .textTheme
-                  .headlineSmall!
-                  .copyWith(fontWeight: FontWeight.bold),
+    return Container(
+      padding: const EdgeInsets.all(20),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            activity.title,
+            style: Theme.of(context)
+                .textTheme
+                .titleLarge!
+                .copyWith(fontWeight: FontWeight.bold),
+          ),
+          const SizedBox(height: 10),
+          RatingBar.builder(
+            initialRating: activity.rating,
+            minRating: 0,
+            direction: Axis.horizontal,
+            ignoreGestures: true,
+            itemCount: 5,
+            itemSize: 20,
+            itemBuilder: (context, _) => const Icon(
+              Icons.star,
+              color: Colors.amber,
             ),
-            const SizedBox(height: 10),
-            RatingBar.builder(
-              initialRating: activity.rating,
-              minRating: 0,
-              direction: Axis.horizontal,
-              ignoreGestures: true,
-              itemCount: 5,
-              itemSize: 20,
-              itemBuilder: (context, _) => const Icon(
-                Icons.star,
-                color: Colors.amber,
+            onRatingUpdate: (rating) {},
+          ),
+          const SizedBox(height: 20),
+          Text(
+            'About',
+            style: Theme.of(context)
+                .textTheme
+                .titleMedium!
+                .copyWith(fontWeight: FontWeight.bold),
+          ),
+          const SizedBox(height: 10),
+          Text(
+            activity.description,
+            textAlign: TextAlign.justify,
+            style: Theme.of(context).textTheme.bodyMedium,
+          ),
+          const SizedBox(height: 20),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                '\Rp ${activity.price}',
+                style: Theme.of(context)
+                    .textTheme
+                    .titleLarge!
+                    .copyWith(fontWeight: FontWeight.bold),
               ),
-              onRatingUpdate: (rating) {},
-            ),
-            const SizedBox(height: 20),
-            Text(
-              'About',
-              style: Theme.of(context)
-                  .textTheme
-                  .bodyLarge!
-                  .copyWith(fontWeight: FontWeight.bold),
-            ),
-            const SizedBox(height: 10),
-            Text(
-              activity.description,
-              textAlign: TextAlign.justify,
-              style: Theme.of(context).textTheme.bodyMedium,
-            ),
-            const Spacer(),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(
-                  '\Rp ${activity.price}',
+              ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.blue,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                ),
+                child: Text(
+                  'Buat Rencana',
                   style: Theme.of(context)
                       .textTheme
-                      .titleLarge!
-                      .copyWith(fontWeight: FontWeight.bold),
+                      .labelLarge!
+                      .copyWith(color: Colors.white),
                 ),
-                ElevatedButton(
-                  onPressed: () {},
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Color.fromARGB(255, 16, 138, 204),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(15.0),
-                    ),
-                  ),
-                  child: Text(
-                    'Buat Rencana',
-                    style: Theme.of(context)
-                        .textTheme
-                        .bodyLarge!
-                        .copyWith(color: Colors.white),
-                  ),
-                ),
-              ],
-            ),
-          ],
-        ),
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
