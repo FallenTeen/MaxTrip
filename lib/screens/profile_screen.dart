23f317cad733d58a7f9b404acd7b268b7a6c64d8
NajibNurdiansyah <najibnurd@gmail.com>
2024-07-01 09:08:27 +0700

    oke

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -53,7 +53,7 @@ class DashboardProfile extends StatelessWidget {
                   ),
                   SizedBox(height: 10),
                   Text(
-                    "Hello, Welcome $userName!",
+                    "Hello, Ambatron!",
                     style: TextStyle(
                       fontSize: 18,
                       color: Colors.white,
